93a9d0232dc9b6dcd81a73bceedd9a829b082003
victor hugo souza vilela <victorsouvilela@hotmail.com>
2020-10-18 11:03:11 -0300

    Inicio da pausa de domingo

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,7 @@
+const map = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
